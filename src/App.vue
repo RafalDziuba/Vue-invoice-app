3e031841691990925ddcba5993988bf4e47bdd34
Rafał Dziuba <Rafal.Dziuba@zetorzeszow.pl>
2022-06-06 16:30:56 +0200

    Navigation and header add

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
-  <router-view />
+  <div>
+    <div class="app flex flex-column">
+      <the-navigation></the-navigation>
+      <div class="app-content flex flex-column">
+        <add-invoice></add-invoice>
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import TheNavigation from "./components/TheNavigation.vue";
+import AddInvoice from "./components/AddInvoice.vue";
+export default {
+  components: {
+    TheNavigation,
+    AddInvoice,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -9,7 +28,19 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
+}
+.app {
   background-color: #141625;
+  min-height: 100vh;
+  @media (min-width: 900px) {
+    flex-direction: row !important;
+  }
+
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
 }
 button,
 .button {
---
+++ src/components/AddInvoice.vue
@@ -0,0 +1,210 @@
+<template>
+  <div
+    @click="checkClick"
+    ref="invoiceWrap"
+    class="invoice-wrap flex flex-column"
+  >
+    <form @submit.prevent="submitForm" class="invoice-content">
+      <h1>New Invoice</h1>
+
+      <div class="bill-from flex flex-column">
+        <h4>Bill from</h4>
+        <div class="input flex flex-column">
+          <label for="billerStreetAddress">Street Address</label>
+          <input
+            required
+            type="text"
+            id="billerStreetAddress"
+            name="billerStreetAddress"
+            v-model="billerStreetAddress"
+          />
+        </div>
+        <div class="location-details flex">
+          <div class="input flex flex-column">
+            <label for="billerCity">City</label>
+            <input
+              required
+              type="text"
+              id="billerCity"
+              name="billerCity"
+              v-model="billerCity"
+            />
+          </div>
+          <div class="input flex flex-column">
+            <label for="billerZipCode">Zip Code</label>
+            <input
+              required
+              type="text"
+              id="billerZipCode"
+              name="billerZipCode"
+              v-model="billerZipCode"
+            />
+          </div>
+          <div class="input flex flex-column">
+            <label for="billerCountry">Country</label>
+            <input
+              required
+              type="text"
+              id="billerCountry"
+              name="billerCountry"
+              v-model="billerCountry"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="bill-to flex flex-column">
+        <h4>Bill to</h4>
+
+        <div class="input flex flex-column">
+          <label for="clientName">Name</label>
+          <input
+            required
+            type="text"
+            id="clientName"
+            name="clientName"
+            v-model="clientName"
+          />
+        </div>
+
+        <div class="input flex flex-column">
+          <label for="clientEmail">E-mail</label>
+          <input
+            required
+            type="text"
+            id="clientEmail"
+            name="clientEmail"
+            v-model="clientEmail"
+          />
+        </div>
+
+        <div class="input flex flex-column">
+          <label for="clientStreetAddress">Street Address</label>
+          <input
+            required
+            type="text"
+            id="clientStreetAddress"
+            name="clientStreetAddress"
+            v-model="clientStreetAddress"
+          />
+        </div>
+
+        <div class="location-details flex">
+          <div class="input flex flex-column">
+            <label for="clientCity">City</label>
+            <input
+              required
+              type="text"
+              id="clientCity"
+              name="clientCity"
+              v-model="clientCity"
+            />
+          </div>
+
+          <div class="input flex flex-column">
+            <label for="clientZipCode">Zip Code</label>
+            <input
+              required
+              type="text"
+              id="clientZipCode"
+              name="clientZipCode"
+              v-model="clientZipCode"
+            />
+          </div>
+
+          <div class="input flex flex-column">
+            <label for="clientCountry">Country</label>
+            <input
+              required
+              type="text"
+              id="clientCountry"
+              name="clientCountry"
+              v-model="clientCountry"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="invoice-work flex flex-column">
+        <div class="payment flex">
+          <div class="input flex flex-column">
+            <label for="invoiceDate">Invoice Date</label>
+            <input
+              disabled
+              type="text"
+              id="invoiceDate"
+              name="invoiceDate"
+              v-model="invoiceDate"
+            />
+          </div>
+          <div class="input flex flex-column">
+            <label for="paymentDueDate">Payment Due</label>
+            <input
+              disabled
+              type="text"
+              id="paymentDueDate"
+              name="paymentDueDate"
+              v-model="paymentDueDate"
+            />
+          </div>
+        </div>
+        <div class="input flex flex-column">
+          <label for="paymentTerms">Payment Terms</label>
+          <select
+            required
+            type="text"
+            id="paymentTerms"
+            name="paymentTerms"
+            v-model="paymentTerms"
+          >
+            <option value="30">Net 30 Days</option>
+            <option value="60">Net 60 Days</option>
+          </select>
+        </div>
+
+        <div class="input flex flex-column">
+          <label for="productDescription">Product Description</label>
+          <input
+            required
+            type="text"
+            id="productDescription"
+            name="productDescription"
+            v-model="productDescription"
+          />
+        </div>
+        <div class="work-items"></div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      billerStreetAddress: null,
+      billerCity: null,
+      billerZipCode: null,
+      billerCountry: null,
+      clientName: null,
+      clientEmail: null,
+      clientStreetAddress: null,
+      clientCity: null,
+      clientZipCode: null,
+      clientCountry: null,
+      invoiceDateUnix: null,
+      invoiceDate: null,
+      paymentTerms: null,
+      paymentDueDateUnix: null,
+      paymentDueDate: null,
+      productDescription: null,
+      invoicePending: null,
+      invoiceDraft: null,
+      invoiceItemList: [],
+      invoiceTotal: 0,
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped></style>
